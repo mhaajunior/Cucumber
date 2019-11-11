3f230bd46d33144c9848d18605d812d6ebbfcc3b
mhaajunior <41276388+mhaajunior@users.noreply.github.com>
2019-11-12 00:10:41 +0700

    Create Account.cpp

---
+++ Account.cpp
@@ -0,0 +1,19 @@
+#include "Account.h"
+
+Account::Account(std::string name, double balance) 
+    : name{name}, balance{balance} {
+}
+
+bool Account::deposit(double amount) {
+    if (amount < 0) 
+        return false;
+    else {
+        balance += amount;
+        return true;
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, const Account &account) {
+    os << "[Account: " << account.name << ": " << account.balance << "]";
+    return os;
+}
